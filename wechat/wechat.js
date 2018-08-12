a108680d4c234434941a12dbecd134e0b382c6bd
刘巧 <liuqiao@liuqiaodeMBP.lan>
2018-08-13 00:29:45 +0800

    增加永久素材上传

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -3,138 +3,304 @@ var request = Promise.promisify(require('request'))
 var config = require('../config.js')
 var util = require('./util.js')
 var fs = require('fs')
+var _ = require('lodash')
 
 var prefix = 'https://api.weixin.qq.com/cgi-bin/'
 var api = {
-	accessToken: prefix + 'token?grant_type=client_credential',
-	upload: prefix + 'media/upload'
+    accessToken: prefix + 'token?grant_type=client_credential',
+    temporary: { // 临时
+        upload: prefix + 'media/upload',
+
+        fetch: prefix + 'media/get', // 获取
+    },
+    permanent: { // 永久 https://api.weixin.qq.com/cgi-bin/material/add_news?access_token=ACCESS_TOKEN
+        upload: prefix + 'material/add_material', // 新增其他类型永久素材
+        uploadNews: prefix + 'material/add_news', // 新增永久图文素材
+        uploadNewsPic: prefix + 'media/uploadimg', // 上传图文消息内的图片获取URL
+
+        fetch: prefix + 'material/get_material', // 获取素材
+        del: prefix + 'material/del_material', // 删除素材
+        update: prefix + 'mmaterial/update_news', // 更新素材
+    }
 }
 
 function Wechat() {
-	// body...
-	// var that = this
-	this.appID = config.wechat.appID
-	this.appSecret = config.wechat.appSecret
-	this.getAccessToken = config.wechat.getAccessToken
-	this.saveAccessToken = config.wechat.saveAccessToken
+    // body...
+    // var that = this
+    this.appID = config.wechat.appID
+    this.appSecret = config.wechat.appSecret
+    this.getAccessToken = config.wechat.getAccessToken
+    this.saveAccessToken = config.wechat.saveAccessToken
 
-	this.fetchAccessToken()
+    this.fetchAccessToken()
 
 }
 
 Wechat.prototype.fetchAccessToken = function() {
-	if (this.access_token && this.expires_in) {
-		if (this.isValidAccessToken(this)) {
-			return Promise.resolve(this)
-		}
-	}
-	this.getAccessToken().then(data => {
-			try {
-				data = JSON.parse(data)
-			} catch (e) {
-				return this.updateAccessToken(data)
-			}
-			console.log('this.isValidAccessToken(data)', this.isValidAccessToken(data))
-			if (this.isValidAccessToken(data)) { // 判断合法性
-				return Promise.resolve(data)
-			} else {
-				return this.updateAccessToken()
-			}
-		})
-		.then((result) => {
-			// console.log(1111111111111, result)
-			// data = JSON.parse(data)
-			this.access_token = result.access_token
-			this.expires_in = result.expires_in
-			this.saveAccessToken(result)
-			return Promise.resolve(result)
-		})
+    if (this.access_token && this.expires_in) {
+        if (this.isValidAccessToken(this)) {
+            return Promise.resolve(this)
+        }
+    }
+    this.getAccessToken().then(data => {
+            try {
+                data = JSON.parse(data)
+            } catch (e) {
+                return this.updateAccessToken(data)
+            }
+            console.log('this.isValidAccessToken(data)', this.isValidAccessToken(data))
+            if (this.isValidAccessToken(data)) { // 判断合法性
+                return Promise.resolve(data)
+            } else {
+                return this.updateAccessToken()
+            }
+        })
+        .then((result) => {
+            // console.log(1111111111111, result)
+            // data = JSON.parse(data)
+            this.access_token = result.access_token
+            this.expires_in = result.expires_in
+            this.saveAccessToken(result)
+            return Promise.resolve(result)
+        })
 }
 
 /*
 	获取token纸
  */
 Wechat.prototype.isValidAccessToken = function(data) {
-	if (!data || !data.access_token || !data.expires_in) {
-		return false
-	}
-	var access_token = data.access_token
-	var expires_in = data.expires_in
-	var now = (new Date().getTime())
+    if (!data || !data.access_token || !data.expires_in) {
+        return false
+    }
+    var access_token = data.access_token
+    var expires_in = data.expires_in
+    var now = (new Date().getTime())
 
-	if (now < expires_in) { // 检查过期时间
-		return true
-	} else {
-		return false
-	}
+    if (now < expires_in) { // 检查过期时间
+        return true
+    } else {
+        return false
+    }
 }
 
 Wechat.prototype.updateAccessToken = function() {
-	var appID = this.appID
-	var appSecret = this.appSecret
-	var url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret
-	console.log("url-----------", url)
-	return new Promise((resolve, reject) => {
-		request({
-			url: url,
-			json: true
-		}).then((res) => { // 对get 和 post 封装过的库
-			console.log(2222222222222222, res)
-			// {
-			//   errcode: 40013,
-			//   errmsg: 'invalid appid hint: [5TkgQA00083612]',
-			//   expires_in: NaN
-			// }
-			var data = res.body
-			var now = (new Date().getTime())
-			var expires_in = now + (data.expires_in - 20) * 1000
+    var appID = this.appID
+    var appSecret = this.appSecret
+    var url = api.accessToken + '&appid=' + appID + '&secret=' + appSecret
+    console.log("url-----------", url)
+    return new Promise((resolve, reject) => {
+        request({
+            url: url,
+            json: true
+        }).then((res) => { // 对get 和 post 封装过的库
+            console.log(2222222222222222, res)
+                // {
+                //   errcode: 40013,
+                //   errmsg: 'invalid appid hint: [5TkgQA00083612]',
+                //   expires_in: NaN
+                // }
+            var data = res.body
+            var now = (new Date().getTime())
+            var expires_in = now + (data.expires_in - 20) * 1000
 
-			data.expires_in = expires_in
-			resolve(data)
-		})
-	})
+            data.expires_in = expires_in
+            resolve(data)
+        })
+    })
 }
 
 // 回复
 Wechat.prototype.reply = function() {
-	var content = this.body // 这个body是undefined
-	var message = this.weixin
-	var xml = util.tpl(content, message)
+    var content = this.body // 这个body是undefined
+    var message = this.weixin
+    var xml = util.tpl(content, message)
 
-	this.status = 200
-	this.type = 'application/xml'
-	this.body = xml
+    this.status = 200
+    this.type = 'application/xml'
+    this.body = xml
 }
 
-// 上传素材
-Wechat.prototype.uploadMaterial = function(type, filepath) {
 
-	var form = {
-		media: fs.createReadStream(filepath)
-	}
+/*
+*	上传素材
+	type: 上传类型
+	material: 上传路径
+	permanent： 判断临时or永久
+*/
+Wechat.prototype.uploadMaterial = function(type, material, permanent) {
 
-	// 
-	return new Promise((resolve, reject) => {
-		this.fetchAccessToken().then((data) => { // 获取
-			var url = api.upload + '?access_token=' + data.access_token + '&type=' + type
-			request({
-				method: 'POST',
-				url: url,
-				formData: form,
-				json: true
-			}).then((res) => { // 
-				var _data = res.body
-				console.log(11111, _data)
-				if (_data) {
-					resolve(_data)
-				} else {
-					throw new Error('upload material fails')
-				}
-			}).catch((err) => {
-				reject(err)
-			})
-		})
-	})
+    var form = {}
+    var uploadUrl = api.temporary.upload
+
+    if (permanent) {
+        uploadUrl = api.permanent.upload
+        _.extend(form, permanent)
+    }
+
+    if (type === 'pic') {
+        uploadUrl = api.permanent.uploadNewsPic
+    }
+
+    if (type === 'news') {
+        uploadUrl = api.permanent.uploadNews
+        form = material
+    } else {
+        form.media = fs.createReadStream(material)
+    }
+
+    // var form = {
+    //     media: fs.createReadStream(material)
+    // }
+
+    // 
+    return new Promise((resolve, reject) => {
+        this.fetchAccessToken().then((data) => { // 获取
+            var url = uploadUrl + '?access_token=' + data.access_token
+            if (!permanent) {
+                url += '&type=' + type
+            } else {
+                form.access_token = data.access_token
+            }
+
+            var options = {
+                method: 'POST',
+                url: url,
+                json: true
+            }
+            if (type === 'news') {
+                options.body = form
+            } else {
+                options.formData = form
+            }
+            request(options).then((res) => { // 
+                var _data = res.body
+                console.log("uploadMaterial", _data)
+                if (_data) {
+                    resolve(_data)
+                } else {
+                    throw new Error('upload material fails')
+                }
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    })
+}
+
+
+Wechat.prototype.fetchMaterial = function(mediaId, type, permanent) {
+
+    var form = {}
+    var fetchUrl = api.permanent.fetch
+
+    if (permanent) {
+        fetchUrl = api.permanent.fetch
+    }
+
+
+
+    // 
+    return new Promise((resolve, reject) => {
+        this.fetchAccessToken().then((data) => { // 获取
+            var form = {
+                media_id: mediaId,
+                access_token: data.access_token
+            }
+            var url = fetchUrl + '?access_token=' + data.access_token
+            var options = {
+                method: 'POST',
+                url: url,
+                json: true
+            }
+
+            if (permanent) {
+                form.media_id = mediaId
+                form.access_token = data.access_token
+                options.body = form
+            } else {
+                if (type === 'video') {
+                    url = url.replace('https://', 'http://')
+                }
+                url += '&media_id=' + mediaId
+            }
+
+            if (type === 'news' || type === 'video') {
+                request(options).then((res) => { // 
+                    var _data = res.body
+                    console.log(11111, _data)
+                    if (_data) {
+                        resolve(_data)
+                    } else {
+                        throw new Error('upload material fails')
+                    }
+                }).catch((err) => {
+                    reject(err)
+                })
+            } else {
+                resolve(url)
+            }
+
+        })
+    })
+}
+
+Wechat.prototype.deleteMaterial = function(mediaId) {
+
+    var form = {
+        media_id: mediaId
+    }
+    return new Promise((resolve, reject) => {
+        this.fetchAccessToken().then((data) => { // 获取
+            var url = api.permanent.del + '?access_token=' + data.access_token + '&media_id=' + mediaId
+
+            request({
+                method: 'POST',
+                url: url,
+                body: form,
+                json: true
+            }).then((res) => { // 
+                var _data = res.body
+                console.log(11111, _data)
+                if (_data) {
+                    resolve(_data)
+                } else {
+                    throw new Error('upload material fails')
+                }
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    })
+}
+
+Wechat.prototype.updateMaterial = function(mediaId, news) {
+
+    var form = {
+        media_id: mediaId
+    }
+    _.extend(form, news)
+    return new Promise((resolve, reject) => {
+        this.fetchAccessToken().then((data) => { // 获取
+            var url = api.permanent.update + '?access_token=' + data.access_token + '&media_id=' + mediaId
+
+            request({
+                method: 'POST',
+                url: url,
+                body: form,
+                json: true
+            }).then((res) => { // 
+                var _data = res.body
+                console.log("updateMaterial", _data)
+                if (_data) {
+                    resolve(_data)
+                } else {
+                    throw new Error('upload material fails')
+                }
+            }).catch((err) => {
+                reject(err)
+            })
+        })
+    })
 }
 
 module.exports = Wechat
--- weixin.js
+++ weixin.js
@@ -5,63 +5,90 @@ var Wechat = require('./wechat/wechat.js')
 var wechatApi = new Wechat()
 
 exports.reply = async function(next) {
-	var message = this.weixin // 获取消息
-	console.log("微信业务逻辑：", message)
-	if (message.MsgType === 'event') {
-		if (message.Event === 'subscribe') { // 关注
-			if (message.EventKey) {
-				console.log('扫二维码进来的：', message.EventKey + '' + message.ticket)
-			}
+    var message = this.weixin // 获取消息
+    console.log("微信业务逻辑：", message)
+    if (message.MsgType === 'event') {
+        if (message.Event === 'subscribe') { // 关注
+            if (message.EventKey) {
+                console.log('扫二维码进来的：', message.EventKey + '' + message.ticket)
+            }
 
-			this.body = "大傻吊，大猪蹄子，你的关注你大哥刘巧的这个公众号\r\n刚刚了解了下微信公众号的开发，做了点机器人回复\r\n试试回复\r\n1\r\n2\r\n3\r\n4\r\n你敢他妈的试试？\r\n这是你的微信生成的ID，我悄悄咪咪告诉你：" + message.FromUserName
+            this.body = "大傻吊，大猪蹄子，你的关注你大哥刘巧的这个公众号\r\n刚刚了解了下微信公众号的开发，做了点机器人回复\r\n试试回复\r\n1\r\n2\r\n3\r\n4\r\n你敢他妈的试试？\r\n这是你的微信生成的ID，我悄悄咪咪告诉你：" + message.FromUserName
 
-		} else if (message.Event === 'unsubscribe') { // 取消关注
-			console.log("已经取消关注")
-			this.body = ""
-		} else if (message.Event === 'location') {
-			this.body = "您的位置是：" + message.Latitude + '/' + message.Longitude + '_' + message.Precison
+        } else if (message.Event === 'unsubscribe') { // 取消关注
+            console.log("已经取消关注")
+            this.body = ""
+        } else if (message.Event === 'location') {
+            this.body = "您的位置是：" + message.Latitude + '/' + message.Longitude + '_' + message.Precison
 
-		} else if (message.Event === 'CLICK') {
-			this.body = "您点击了菜单：" + message.EventKey
-		} else if (message.Event === 'SCAN') {
-			console.log("关注后扫二维码：" + message.EventKey + '' + message.Ticket)
-			this.body = '看到你扫了一下'
-		} else if (message.Event === 'VIEW') {
-			this.body = "您点击了菜单中的链接：" + message.EventKey
-		}
-	} else if (message.MsgType === 'text') {
-		var content = message.Content
-		var reply = '你说的是：' + message.Content + '。但是太复杂了，听不懂！'
-		if (content === '1') {
-			reply = "这是1的反应"
-		} else if (content === '2') {
-			reply = "这是2的反应"
-		} else if (content === '3') {
-			reply = "这是3的反应"
-		} else if (content === '4') {
-			reply = [{
-				title: '小伙，你太赞了！！！',
-				description: '关于你的自我认识你需要好好了解下，真的哦，不是我欺负你。',
-				picUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/pVcKPKpQwcN8DWaUUAYtOdhgd2yoFPl1Ke21pe1ibmsJtb3sZNDnAjrtsz48aSRyfPX3quhepWwicXsOB8DzIYicw/0',
-				url: 'https://www.bilibili.com/',
-			}]
-		} else if (content === '5') {     // 新增临时素材图片
-			var data = await wechatApi.uploadMaterial('image', __dirname + '/2.png')
-			reply = {
-				type: 'image',
-				mediaId: data.media_id
-			}
-		} else if (content === '6') {    // 新增临时素材视频
-			var data = await wechatApi.uploadMaterial('video', __dirname + '/2.mp4')
-			reply = {
-				type: 'video',
-				title: '这个是个视频文件',
-				description: '这是个视频简介',
-				mediaId: data.media_id
-			}
-		}
-		this.body = reply
-	}
+        } else if (message.Event === 'CLICK') {
+            this.body = "您点击了菜单：" + message.EventKey
+        } else if (message.Event === 'SCAN') {
+            console.log("关注后扫二维码：" + message.EventKey + '' + message.Ticket)
+            this.body = '看到你扫了一下'
+        } else if (message.Event === 'VIEW') {
+            this.body = "您点击了菜单中的链接：" + message.EventKey
+        }
+    } else if (message.MsgType === 'text') {
+        var content = message.Content
+        var reply = '你说的是：' + message.Content + '。但是太复杂了，听不懂！'
+        if (content === '1') {
+            reply = "这是1的反应"
+        } else if (content === '2') {
+            reply = "这是2的反应"
+        } else if (content === '3') {
+            reply = "这是3的反应"
+        } else if (content === '4') {
+            reply = [{
+                title: '小伙，你太赞了！！！',
+                description: '关于你的自我认识你需要好好了解下，真的哦，不是我欺负你。',
+                picUrl: 'http://mmbiz.qpic.cn/mmbiz_jpg/pVcKPKpQwcN8DWaUUAYtOdhgd2yoFPl1Ke21pe1ibmsJtb3sZNDnAjrtsz48aSRyfPX3quhepWwicXsOB8DzIYicw/0',
+                url: 'https://www.bilibili.com/',
+            }]
+        } else if (content === '5') { // 新增临时素材图片
+            var data = await wechatApi.uploadMaterial('image', __dirname + '/2.png')
+            reply = {
+                type: 'image',
+                mediaId: data.media_id
+            }
+        } else if (content === '6') { // 新增临时素材视频
+            var data = await wechatApi.uploadMaterial('video', __dirname + '/2.mp4')
+            reply = {
+                type: 'video',
+                title: '这个是个视频文件',
+                description: '这是个视频简介',
+                mediaId: data.media_id
+            }
+        } else if (content === '7') { // 永久素材的测试
+            var picData = await wechatApi.uploadMaterial('image', __dirname + '/2.png', {}) // 上传永久
+            var media = {
+                articles: [{
+                    title: '哈哈哈',
+                    thumb_media_id: picData.media_id,
+                    author: 'stone',
+                    digest: '没有假药',
+                    show_cover_pic: 1,
+                    content: '没有内推',
+                    content_source_url: 'https://github.com',
+                }]
+            }
+            data = await wechatApi.uploadMaterial('news', media, {})
+            data = await wechatApi.fetchMaterial(data.media_id, 'news', {})
+            console.log("获取的信息", data)
+            var items = data.news_item
+            var news = []
+            items.forEach(function(item) {
+                news.push({
+                    title: item.title,
+                    description: item.digest,
+                    picUrl: picData.url,
+                    url: item.url,
+                })
+            })
+            reply = news
+        }
+        this.body = reply
+    }
 
-	await next()
+    await next()
 }
